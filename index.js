1820e2411360570e08905d673d885a386aa2281f
divyasajjan1 <99633534+divyasajjan1@users.noreply.github.com>
2022-04-07 12:42:20 -0400

    Initial upload of dice game files.

---
+++ index.js
@@ -0,0 +1,24 @@
+//dice 1
+var randomNumber1 = Math.floor(Math.random()*6) + 1;    //1-6
+
+var randomDiceImage1 = "images/dice" + randomNumber1 + ".png";  //images/dice1.png - images/dice6.png
+
+document.querySelectorAll("img")[0].setAttribute("src", randomDiceImage1); 
+
+//dice 2
+var randomNumber2 = Math.floor(Math.random()*6) + 1;
+
+var randomDiceImage2 = "images/dice" + randomNumber2 + ".png"; 
+
+document.querySelectorAll("img")[1].setAttribute("src", randomDiceImage2);
+  
+//who wins
+if (randomNumber1 > randomNumber2){
+    document.querySelector("h1").innerHTML = "🚩Player 1 wins";
+} else if (randomNumber2 > randomNumber1) {
+    document.querySelector("h1").innerHTML = "Player 2 wins 🚩";
+} else {
+    document.querySelector("h1").innerHTML = "It's a tie!";
+}
+
+
